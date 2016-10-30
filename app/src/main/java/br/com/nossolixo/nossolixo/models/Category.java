2355aa2ae13a8b85bbf56413803fe5c53d5b7691
Thiago Guimaraes <thiagogsr@gmail.com>
2016-10-30 15:01:52 -0300

    Category model

---
+++ app/src/main/java/br/com/nossolixo/nossolixo/models/Category.java
@@ -0,0 +1,31 @@
+package br.com.nossolixo.nossolixo.models;
+
+public class Category {
+    private String id;
+    private String name;
+    private String color;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
