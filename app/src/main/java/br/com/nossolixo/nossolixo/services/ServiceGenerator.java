46aba452052724ab3d10f71bdae06713b056b4e1
Thiago Guimaraes <thiagogsr@gmail.com>
2016-10-30 15:03:40 -0300

    Service generetor to connect with api

---
+++ app/src/main/java/br/com/nossolixo/nossolixo/services/ServiceGenerator.java
@@ -0,0 +1,23 @@
+package br.com.nossolixo.nossolixo.services;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServiceGenerator {
+    private static final String API_BASE_URL = "https://nossolixo-api.herokuapp.com/v1/";
+    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+    private ServiceGenerator() {
+    }
+
+    private static Retrofit.Builder builder =
+            new Retrofit.Builder()
+                    .baseUrl(API_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create());
+
+    public static <S> S createService(Class<S> serviceClass) {
+        Retrofit retrofit = builder.client(httpClient.build()).build();
+        return retrofit.create(serviceClass);
+    }
+}
