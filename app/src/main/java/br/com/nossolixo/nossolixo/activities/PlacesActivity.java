f63094f60dda2a38d3dc31cf09226eb29d6ed50d
Thiago Guimaraes <thiagogsr@gmail.com>
2016-10-30 15:09:27 -0300

    Animate map to current location and add markers from api

--- app/src/main/java/br/com/nossolixo/nossolixo/activities/PlacesActivity.java
+++ app/src/main/java/br/com/nossolixo/nossolixo/activities/PlacesActivity.java
@@ -1,48 +1,144 @@
 package br.com.nossolixo.nossolixo.activities;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
 import br.com.nossolixo.nossolixo.R;
+import br.com.nossolixo.nossolixo.models.Place;
+import br.com.nossolixo.nossolixo.services.PlaceService;
+import br.com.nossolixo.nossolixo.services.ServiceGenerator;
+import br.com.nossolixo.nossolixo.utils.PermissionUtils;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class PlacesActivity extends FragmentActivity implements OnMapReadyCallback {
+public class PlacesActivity extends AppCompatActivity
+        implements
+        GoogleMap.OnMyLocationButtonClickListener,
+        OnMapReadyCallback,
+        ActivityCompat.OnRequestPermissionsResultCallback {
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_places);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    private void enableMyLocation() {
+        if (ableToLocale()) {
+            mMap.setMyLocationEnabled(true);
+            animateToMyLocation();
+        } else if (mMap != null) {
+            PermissionUtils.requestPermission(this,
+                    LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions,
+                                                grantResults,
+                                                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            enableMyLocation();
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMyLocationButtonClickListener(this);
+        enableMyLocation();
+        setupMap();
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        PlaceService client = ServiceGenerator.createService(PlaceService.class);
+        Call<List<Place>> call = client.listPlaces();
+        call.enqueue(new Callback<List<Place>>() {
+            @Override
+            public void onResponse(Call<List<Place>> call, Response<List<Place>> response) {
+                if (response.isSuccessful()) {
+                    List<Place> places = response.body();
+                    for (Place place : places) {
+                        mMap.addMarker(new MarkerOptions()
+                                .position(place.getLatLng())
+                                .title(place.getName()));
+                    }
+                } else {
+                    Log.d("Error", String.valueOf(response.raw()));
+                }
+                animateToMyLocation();
+            }
+
+            @Override
+            public void onFailure(Call<List<Place>> call, Throwable t) {
+                Log.d("Error", t.getMessage());
+            }
+        });
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        animateToMyLocation();
+        return false;
+    }
+
+    private void animateToMyLocation() {
+        if (!ableToLocale()) {
+            return;
+        }
+        LocationManager locationManager =
+                (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        String provider = locationManager.getBestProvider(criteria,true);
+        Location mostRecentLocation = locationManager.getLastKnownLocation(provider);
+        LatLng myLocation = new LatLng(mostRecentLocation.getLatitude(),
+                mostRecentLocation.getLongitude());
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 12));
+    }
+
+    private boolean ableToLocale() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void setupMap() {
+        UiSettings mUiSettings = mMap.getUiSettings();
+        mUiSettings.setZoomControlsEnabled(true);
+        mUiSettings.setScrollGesturesEnabled(true);
+        mUiSettings.setZoomGesturesEnabled(true);
+        mUiSettings.setRotateGesturesEnabled(true);
     }
 }
