2a88c95d8af9a1ac4a4aa4a49769be871cc21ebe
Thiago Guimaraes <thiagogsr@gmail.com>
2016-10-30 15:01:58 -0300

    Place model

---
+++ app/src/main/java/br/com/nossolixo/nossolixo/models/Place.java
@@ -0,0 +1,138 @@
+package br.com.nossolixo.nossolixo.models;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+public class Place {
+    private String id;
+    private String name;
+    private String description;
+    private String street;
+    private String number;
+    private String destrict;
+    private String lat;
+    private String lng;
+    private String email;
+    private String site;
+    private Boolean approved;
+    private String city;
+    private String state;
+    private List<Category> categories;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDestrict() {
+        return destrict;
+    }
+
+    public void setDestrict(String destrict) {
+        this.destrict = destrict;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public Boolean getApproved() {
+        return approved;
+    }
+
+    public void setApproved(Boolean approved) {
+        this.approved = approved;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public LatLng getLatLng() {
+        return new LatLng(Double.parseDouble(lat), Double.parseDouble(lng));
+    }
+}
