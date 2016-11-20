67cd953f22cb63f85986d5c12e21f9992f9a6610
Thiago Guimaraes <thiagogsr@gmail.com>
2016-11-20 15:21:05 -0300

    Add method to find a place

--- app/src/main/java/br/com/nossolixo/nossolixo/services/PlaceService.java
+++ app/src/main/java/br/com/nossolixo/nossolixo/services/PlaceService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import br.com.nossolixo.nossolixo.models.Place;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface PlaceService {
@@ -13,4 +14,7 @@ public interface PlaceService {
 
     @GET("places")
     Call<List<Place>> listPlacesByCategory(@Query("category") String category);
+
+    @GET("place/{id}")
+    Call<Place> find(@Path("id") String placeId);
 }
