9a7ec86b899ecb4d2608aaee3b957c48eb5d4631
Thiago Guimaraes <thiagogsr@gmail.com>
2016-11-20 20:03:24 -0300

    Improve about activity

--- app/src/main/java/br/com/nossolixo/nossolixo/activities/AboutActivity.java
+++ app/src/main/java/br/com/nossolixo/nossolixo/activities/AboutActivity.java
@@ -1,15 +1,34 @@
 package br.com.nossolixo.nossolixo.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import br.com.nossolixo.nossolixo.R;
 
 public class AboutActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(R.string.about);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpTo(this, new Intent(this, MainActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
